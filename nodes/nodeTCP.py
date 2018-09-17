4dbc64f59f3b837e4644a0d52d140d6447030547
Alejandro Chacon <geovanny.chacon@ucr.ac.cr>
2018-09-16 21:12:35 -0600

    Node update

--- nodes/nodeTCP.py
+++ nodes/nodeTCP.py
@@ -2,10 +2,12 @@ from socket import *
 
 class nodeTCP:
 
-    #constructor del nodo
+    # constructor del nodo
     def __init__(self, ip, port):
-        #ip del nodo creado
+        # ip del nodo creado
         self.ip = ip
-        #puerto habilitado del nodo
+        # puerto habilitado del nodo
         self.port = port
+
+
 
