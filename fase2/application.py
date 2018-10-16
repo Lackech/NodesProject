9be360cd7986136e140d4f89dfb68c9699e9b532
Ducan_Krake <fabian.alvarezchevez@ucr.ac.cr>
2018-10-15 19:05:20 -0600

    Version del router mejorada y toda la parte de direccionamiento

--- fase2/application.py
+++ fase2/application.py
@@ -10,21 +10,23 @@ class Application:
         self.router = Router()
         self.node = None
 
-        self.mainMessage = "Please choose a type of connection:"
+        self.greetingMessage = ("Welcome to our Face 2 application!!"
+                                "\tLets proceed and create your UDP node...")
 
-        self.connectionTypeMessage = "\tWrite pseudoBGP(TCP) or intAS(UDP): "
-        self.TcpMessage = "Great!! You choosed to create a TCP node"
-        self.UdpMessage = "Interesting...You choosed to create a UDP node"
+        self.nodeIpMessage = "\tWrite your Node's IP: "
 
-        self.creatingMsgMessage = "Lets write the message!"
-        self.creatingConnectionMessage = "Lests write your Bruh's information"
-        self.unsuccessfulConnectionMessage = "WARNING -- Unable to create connection"
+        self.nodePortMessage = "\tWrite your Node's Port: "
+        self.nodePortMessage = "\tWrite your Node's Port: "
+        self.nodeMascaraMessage = "\tWrite your Node's Mascara: "
 
-        self.numberOfElementsMessage = "\tSelect the number of elements: "
-        self.nodeIpMessage = "\tWrite a valid Node's IP: "
-        self.nodePortMessage = "\tWrite a valid Node's Port: "
-        self.nodeMascaraMessage = "\tWrite a valid Node's Mascara: "
-        self.nodeCostMessage = "\tWrite a valid Node's Cost: "
+        self.connectingPortMessage = "\tWrite the new connection Node's Port: "
+        self.connectingPortMessage = "\tWrite new connection Node's Port: "
+        self.connectingMascaraMessage = "\tWrite new connection Node's Mascara: "
+
+        self.warningMessage = "\tWARNING -- \""
+        self.invalidIpMessage = "\" -- is not a valid IP"
+        self.invalidPortMessage = "\" -- is not a valid Port"
+        self.invalidMascarMessage = "\" -- is not a valid Mascara"
 
         self.nodeMessage = ("Please write the number of the one you want to do:\n"
                             "\t1. Send message\n"
@@ -33,7 +35,6 @@ class Application:
                             "\t4. Create a catastrophe(Exit)\n"
                             "\tWrite your option here --> ")
 
-        self.errorMessage = "WARNING -- Be sure to choose a valid option"
 
 
 
@@ -44,26 +45,11 @@ class Application:
 
 
     def nodeCreation(self):
-        self.interface.showMessage(self.mainMessage)
-        nodeType = 0
+        self.interface.showMessage(self.greetingMessage)
 
-        while nodeType is 0:
-            nodeType = self.isValid(self.interface.getInput(self.connectionTypeMessage), ["pseudoBGP", "intAS"])
+        nodeAddress = self.getNodeInformation()
+        self.node = NodeUdp(nodeAddress)
 
-            if nodeType is 0:
-                self.interface.showMessage(self.errorMessage)
-
-        if nodeType is 1:
-            self.interface.showMessage(self.TcpMessage)
-            nodeAddress = self.getNodeInformation()
-            self.node = NodeTcp(nodeAddress)
-            self.nodeExecution()
-
-        else:
-            if nodeType is 2:
-                self.interface.showMessage(self.UdpMessage)
-                nodeAddress = self.getNodeInformation()
-                self.node = NodeUdp(nodeAddress)
 
 
 
@@ -122,9 +108,9 @@ class Application:
 
     def optainNodeIp(self):
         nodeIp = ""
-        while self.router.validIp(nodeIp) is False:
+        while self.router.validIp(nodeIp)[0] is False:
             nodeIp = self.interface.getInput(self.nodeIpMessage)
-            if self.router.validIp(nodeIp) is False:
+            if self.router.validIp(nodeIp)[0] is False:
                 self.interface.showMessage(self.errorMessage)
 
         return nodeIp
@@ -132,40 +118,22 @@ class Application:
     def optainNodePort(self):
         nodePort = -1
         while self.router.validPort(nodePort) is False:
-            nodePort = int(self.interface.getInput(self.nodePortMessage))
+            nodePort = self.interface.getInput(self.nodePortMessage)
+
             if self.router.validPort(nodePort) is False:
-                self.interface.showMessage(self.errorMessage)
+                self.interface.showMessage(self.warningMessage + nodePort + self.invalidPortMessage)
 
         return nodePort
 
-    def optainNodeMascara(self):
+    def optainNodeMascara(self,tipoRed):
         nodeMascara = -1
-        while self.router.validMascar(nodeMascara) is False:
-            nodeMascara = int(self.interface.getInput(self.nodeMascaraMessage))
-            if self.router.validMascar(nodeMascara) is False:
-                self.interface.showMessage(self.errorMessage)
+        while self.router.validMascara(nodeMascara,tipoRed) is False:
+            nodeMascara = self.interface.getInput(self.nodeMascaraMessage)
+
+            if self.router.validMascara(nodeMascara,tipoRed) is False:
+                self.interface.showMessage(self.warningMessage + nodeMascara + self.invalidMascaraMessage)
 
         return int(nodeMascara)
-
-    def optainNodeCost(self):
-        nodeCost = -1
-        while nodeCost < 0:
-            nodeCost = int(self.interface.getInput(self.nodeCostMessage))
-            if nodeCost < 0:
-                self.interface.showMessage(self.errorMessage)
-
-        return nodeCost
-
-    def optainValidNumber(self):
-        numberOfElements = -1
-        while numberOfElements <= 0:
-            numberOfElements = int(self.interface.getInput(self.numberOfElementsMessage))
-            if numberOfElements <= 0:
-                self.interface.showMessage(self.errorMessage)
-
-        return numberOfElements
-
-
 
     def getNodeInformation(self):
         nodeIp = self.optainNodeIp()
@@ -176,24 +144,4 @@ class Application:
 
 
     def optainSendingMessage(self):
-        self.interface.showMessage(self.creatingMsgMessage)
-        messageList = []
-
-        numberOfElements = self.optainValidNumber()
-        messageList.append(numberOfElements)
-
-        for i in range(0, numberOfElements):
-        #Optenemos la informacion que va a contener el mensaje
-
-            #Optienemos el IP
-            nodeIp = self.optainNodeIp()
-            #Optenemos la mascara
-            nodeMascara = self.optainNodeMascara()
-            #Optenemos el costo
-            nodeCost = self.optainNodeCost()
-
-            #Lo agregamos a la lista qu tiene la informacion del mensaje
-            data = (nodeIp,nodeMascara,nodeCost)
-            messageList.append(data)
-
-        return messageList
+        string = "holamundo"
--- fase2/router.py
+++ fase2/router.py
@@ -1,36 +1,85 @@
+redA = 0
+redLocal = 1
+redB = 2
+redC = 3
+
+
 class Router:
 
-    def validIp(self, ipAdress):
+    def validIp(self, ip):
         valid = True
-        if ipAdress != 'localhost':
-            ipSection = ipAdress.split('.')
+        tipoRed = redC
+        if ip != 'localhost':
+            ipSection = ip.split('.')
 
             if len(ipSection) is 4:
-                for section in ipSection:
-                    if int(section) < 0 or int(section) > 255:
+                try:
+                    if int(ipSection[0]) <= 0 or int(ipSection[0])  > 223:
                         valid = False
-                        break
+                    elif int(ipSection[0]) < 127:
+                        tipoRed = redA
+                    elif int(ipSection[0]) == 127:
+                        tipoRed = redLocal
+                    elif int(ipSection[0]) < 192:
+                        tipoRed = redB
+
+
+                    if valid:
+                        for section in ipSection:
+                            if int(section) < 0 or int(section) > 255:
+                                valid = False
+
+                        if valid:
+                            if ipSection[3] == 0:
+                                if tipoRed == redC:
+                                    valid = False
+                                elif ipSection[2] == 0:
+                                    if tipoRed == redB:
+                                        valid = False
+                                    elif ipSection[1] == 0:
+                                        if tipoRed == redA:
+                                            valid = False
+                except:
+                    valid = False
             else:
+                valid = False
+
+        return (valid,tipoRed)
+
+
+
+    def validPort(self, port):
+        valid = True
+        portInt = 0
+        try:
+            portInt = int(port)
+        except:
+            valid = False
+
+        if valid:
+            if portInt < 1024 or portInt > 49151:
                 valid = False
 
         return valid
 
 
 
-    def validPort(self, portAddress):
+    def validMascara(self, mascara, tipoRed):
         valid = True
-
-        if portAddress < 1024 or portAddress > 60000:
+        mascaraInt = 0
+        try:
+            mascaraInt = int(mascara)
+        except:
             valid = False
 
-        return valid
-
-
-
-    def validMascar(self, mascara):
-        valid = True
-
-        if mascara < 1 or mascara > 31:
-            valid = False
+        if valid:
+            if mascara < 8 or mascara > 31:
+                valid = False
+            elif tipoRed == redB:
+                if mascara < 16:
+                    valid = False
+            elif tipoRed == redC:
+                if mascara < 24:
+                    valid = False
 
         return valid
