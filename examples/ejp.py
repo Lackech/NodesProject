57c89cf23a8a2b445c1d5da7209f0b3eb336a5ba
Ducan_Krake <fabian.alvarezchevez@ucr.ac.cr>
2018-10-17 02:21:48 -0600

    Bitnator casi finalizado

--- examples/ejp.py
+++ examples/ejp.py
@@ -1,16 +1,17 @@
-code = bytearray()
-for i in range(0,20):
-    code += i.to_bytes(2,'big')
-    print(code)
-    decode = int.from_bytes(code,'big')
-    #print(decode)
-    string = str(i) + 'adf'
-    #print(string)
-    print(len(code))
+listaA = []
+listaB = []
 
+listaA.append('a')
+listaA.append('b')
+listaA.append('c')
+listaB.append('a')
+listaB.append('b')
+listaB.append('c')
+listaA.extend(listaB)
 
-for i in range(0,20):
-    print(code[i*2]*256 + code[i*2+1])
+for i in listaA:
+    print(i)
+
 
 
 
--- fase2/bitnator.py
+++ fase2/bitnator.py
@@ -1,48 +1,62 @@
 from fase2.nodeUDP import *
 
+FLAG = 3
+
 class Bitnator:
 
     def __init__(self, node):
         self.node = node
 
-    def bitDecrypt(self, packetMessage, senderAddress):
-        # Optenemos la cantidad de mensajes recibidos
-        numberOfElements = packetMessage[0]*256 + packetMessage[1]
+    def encryptConnectPacket(self,myAddress,otherAddress,SYN,ACK):
+        packet = bytearray()
 
-        for i in range(0, numberOfElements):
-            # Optenemos el Ip
-            num1 = packetMessage[i*8+2]
-            num2 = packetMessage[i*8+3]
-            num3 = packetMessage[i*8+4]
-            num4 = packetMessage[i*8+5]
+        packet += self.encryptAddress(myAddress)
+        packet += self.encryptAddress(otherAddress)
 
-            address = str(num1) + "." + str(num2) + "." + str(num3) + "." + str(num4)
+        packet += int(0).to_bytes(1, 'big')
 
-            # Optenemos la máscara
-            mask = packetMessage[i*8+6]
-
-            # Optenemos el costo
-            cost = packetMessage[i*8+7]*65536 + packetMessage[i*8+8]*256 + packetMessage[i*8+9]
-
-            # Guardamos en la tabla de alcanzabilidad
-            self.node.saveDataTable(address,mask,cost,senderAddress)
+        packet += SYN.to_bytes(1, 'big')
+        packet += ACK.to_bytes(1, 'big')
 
 
+    def encryptAddress(self,addres):
+        encriptedAddres = bytearray()
 
-    # Método para codificar mensaje
-    def bitEncript(self,messageList):
-        numberOfElements = messageList.pop(0)
-        encriptedMessage = bytearray()
-        encriptedMessage += numberOfElements.to_bytes(2,'big')
+        #Encripta el Ip
+        ipParts = addres[0].split('.')
+        for s in ipParts:
+            encriptedAddres += int(s).to_bytes(1, 'big')
 
-        for i in range(0, numberOfElements):
-            message = messageList.pop()
-            ipAdress = message[0]
-            ipParts = ipAdress.split('.')
-            for s in ipParts:
-                encriptedMessage += int(s).to_bytes(1,'big')
+        #Encripta el Puerto
+        encriptedAddres += addres[1].to_bytes(2, 'big')
 
-            encriptedMessage += message[1].to_bytes(1,'big')
-            encriptedMessage += message[2].to_bytes(3,'big')
+        return encriptedAddres
 
-        return encriptedMessage
+    def decryptPacket(self, packet):
+        packetParts = []
+
+        #Decodifica la parte del header
+        packetParts.extend(self.decryptHeader(packet))
+
+        #Decodifica la bandera
+        packetParts.append(packet[13])
+
+        packetParts.append(packet[14])
+        packetParts.append(packet[15])
+
+    def decryptHeader(self,packet):
+        headerParts = []
+
+        for i in range(0,2):
+            num1 = packet[i*6]
+            num2 = packet[i*6+1]
+            num3 = packet[i*6+2]
+            num4 = packet[i*6+3]
+
+            addressIp = str(num1) + "." + str(num2) + "." + str(num3) + "." + str(num4)
+
+            addressPort = packet[i*6+4]*256 + packet[i*6+5]
+
+            headerParts.append((addressIp,addressPort))
+
+        return headerParts
