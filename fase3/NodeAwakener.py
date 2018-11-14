54e6baaf2523370be42e7fedd116ec3531cd2919
Ducan_Krake <fabian.alvarezchevez@ucr.ac.cr>
2018-11-13 21:51:26 -0600

    Se inicia con la creación de la Clase NodeUDP, se continua con la Clase NodeAwakener, modificaciones al Node y el Bitnator

--- fase3/NodeAwakener.py
+++ fase3/NodeAwakener.py
@@ -1,5 +1,6 @@
 from socket import *
 from fase3.Node import *
+from fase3.NodeUDP import *
 import csv
 
 
@@ -11,12 +12,8 @@ class NodeAwakener(Node):
         # Llamamos al constructor del padre, para guardar el address del activador del nodo
         Node.__init__(NODE_AWAKENER_ADDRESS,NODE_AWAKENER_MASCARA)
 
-        socketServer = socket(AF_INET, SOCK_DGRAM)
-        socketServer.bind(NODE_AWAKENER_ADDRESS)
-
-        self.listener = threading.Thread(name='daemon', target=self.listenForAnswers())
-        self.listener.setDaemon(True)
-        self.listener.start()
+        self.socketServer = socket(AF_INET, SOCK_DGRAM)
+        self.socketServer.bind(NODE_AWAKENER_ADDRESS)
 
 
 
@@ -58,11 +55,16 @@ class NodeAwakener(Node):
     def awakeNode(self,nodeAddress,nodeMascara):
         success = True
 
+        # Verificamos que el ip este bien
         information = self.router.validIp(nodeAddress[0])
         if information[0] == True:
+            # Verificamos que tanto el puesto como la mascara sean validos
             if self.router.validPort(nodeAddress[1]) and self.router.validMascara(nodeMascara,information[1]):
                 # Creamos el nodo
-                pass
+                NewNode = NodeUDP(nodeAddress,nodeMascara)
+
+                # Verificamos si el nodo se creó correctamente
+                success = self.verifyExistence(nodeAddress)
             else:
                 success = False
         else:
@@ -75,13 +77,62 @@ class NodeAwakener(Node):
 
 
     # Se encarga de esperar la respuesta de los nodos que despertó
-    def listenForAnswers(self):
-        while self.alive:
+    def listenForAnswers(self,checkingAddress):
+        success = False
+        try:
+            #Esperamos el paquete
             packetMessage, clientAddress = self.serverSocket.recvfrom(2048)
 
+            # Desencriptamos el mensaje
+            decryptedMessage = self.bitnator.decrypt(packetMessage)
+
+            # Verificamos si el mensaje es una respuesta
+            if decryptedMessage[SOURCE_IP] == checkingAddress[IP] and decryptedMessage[SOURCE_PORT] == checkingAddress[
+                PORT] and decryptedMessage[HELLO_ACK] == 1:
+
+                success = True
+        except:
+            # Si ocurre un erro no hacemos nada
+            pass
+
+        return success
 
 
 
 
 
     # Se encarga de verificar sí el nodo se despertó correctamente
+    def verifyExistence(self,otherAddress):
+        success = False
+        #Crea la conexión con el servidor
+        clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+        #Envía un mensaje codificado
+        encryptedMessage = self.bitnator.encrypt(
+            addressOrigen = otherAddress,
+            ps = 0,
+            rs = 0,
+            sa = 1,
+            saAck = 0,
+            act = 0,
+            actAck = 0,
+            type = 0,
+            tv = 0,
+            data = "empty"
+        )
+
+        try:
+            # Tratamos de enviar el mensaje
+            clientSocket.sendto(encryptedMessage,otherAddress)
+
+            # Esperamos una respuesta del nodo
+            success = self.listenForAnswers(otherAddress)
+
+        except:
+            # No hacemo nada
+            pass
+
+        # Cerramos la conexión
+        clientSocket.close()
+
+        return success
--- fase3/NodeUDP.py
+++ fase3/NodeUDP.py
@@ -1,15 +1,16 @@
 from socket import *
 from fase3.Node import *
 
-class NodeUDP:
+class NodeUDP(Node):
 
-    def __init__(self):
+    def __init__(self,address,mascara):
         # Llamamos al constructor del padre, para guardar el address del activador del nodo
-        Node.__init__(ADDRESS, MASCARA) # Ver como manejamos las direcciones aca
+        Node.__init__(address, mascara) # Ver como manejamos las direcciones aca
 
         # Tablas
         self.reachabilityTable = {}
         self.neighborTable = {}
+
         # Candados
         self.lockReach = threading.Lock()
         self.lockLog = threading.Lock()
@@ -18,6 +19,60 @@ class NodeUDP:
         # Decimos que el nodo está vivo
         self.alive = True
 
-        self.socket = socket(AF_INET, SOCK_DGRAM)
-        self.socket.bind(("", self.port))
+        # Creamos el socket servido del nodo
+        self.socketServer = socket(AF_INET, SOCK_DGRAM)
+        self.socketServer.bind(self.ad)
 
+        self.listener = threading.Thread(name='daemon', target=self.listen)
+        self.listener.setDaemon(True)
+        self.listener.start()
+
+
+
+
+
+    # Metodo que se encarga de recibir todos los mensajes que le llegan al nodo
+    def listen(self):
+        while self.alive:
+            # Recibimos el paquete
+            packetMessage, clientAddress = self.serverSocket.recvfrom(2048)
+
+            # Desencrptamos el paquete
+            decrytedMessage = self.bitnator.decrypt(packetMessage)
+
+            # Creamo un hilo que analiza el paquete
+            thread = threading.Thread(name='Analizador', target=self.analysMessage, args=decrytedMessage)
+            thread.setDaemon(True)
+            thread.start()
+
+
+        self.serverSocket.close()
+
+
+
+
+
+    # Se encarga de analizar el paquete y dependiendo de las banderas que vienen activadas varía lo que hace
+    def analysMessage(self,decryptedMessage):
+        # Ve cuales son las banderas que están activadas, y dependiendo de esto hace algo diferente
+        if decryptedMessage[SERVER_ACK] == 1:
+            # Entramos en el caso de que el servidor le haya devuelto una respuesta con la información de los vecinos
+            pass
+        elif decryptedMessage[HELLO] == 1:
+            # Entramos en el caso donde el despachador esta verificando sí el nodo está despierto o no
+            pass
+        elif decryptedMessage[UPDATE] == 1:
+            # Entramos en el caso donde el mensaje recibido es una actualización de la tabla de alcanzabilidad
+            pass
+        elif decryptedMessage[TYPE] == 1:
+            # Entrams en el caso donde lo recibido es un mensaje de datos
+            pass
+
+
+
+
+
+    # Método para borrar un nodo
+    def kill(self):
+        #Matamos el servidor
+        self.alive = False
